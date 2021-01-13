9e26a6cd3c1c65a9f08f06428fa679028a0639e7
Saile Tobias <bsailt@bbcag.ch>
2021-01-13 14:51:56 +0100

    first commit

---
+++ src/main/java/Aufgabe1.java
@@ -0,0 +1,15 @@
+public class Aufgabe1 {
+    public static void main(String[] args) {
+        int a = -1;
+		double b = 10.0;
+		// Dies ist ein Kommentar
+        double resultat = a + b;
+                String text = "Das Resultat ist: " + resultat;
+
+        if (resultat < 10.0) {
+            System.out.println("Gratulation!");
+        }	else {
+                System.out.println(text);
+		}
+    }
+}
